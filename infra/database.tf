7fc850da379232d24df406f3c354e0a28a16fb0b
Oskari Järvinen <oskari.jarvinen@pinecone.team>
2023-11-12 05:31:53 +0200

    hallusinoin

--- infra/database.tf
+++ infra/database.tf
@@ -1,11 +1,11 @@
-resource "upcloud_managed_database_postgresql" "db" {
-  name  = "postgres"
-  plan  = "1x1xCPU-2GB-25GB"
-  title = "postgres"
-  zone  = var.zone
-  properties {
-    timezone       = "Europe/Helsinki"
-    admin_username = "admin"
-    admin_password = "bonsibuddy"
-  }
-}
+#resource "upcloud_managed_database_postgresql" "db" {
+#  name  = "postgres"
+#  plan  = "1x1xCPU-2GB-25GB"
+#  title = "postgres"
+#  zone  = var.zone
+#  properties {
+#    timezone       = "Europe/Helsinki"
+#    admin_username = "admin"
+#    admin_password = "bonsibuddy"
+#  }
+#}
