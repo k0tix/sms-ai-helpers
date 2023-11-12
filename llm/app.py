22b00a0079bfd88c911761a9d2bbaf67d8752133
Roope Kausiala <roope.kausiala@aktia.fi>
2023-11-12 03:09:45 +0200

    change model

--- llm/app.py
+++ llm/app.py
@@ -5,12 +5,13 @@ import os
 
 app = Flask(__name__)
 #print(f"DIRRRR: {os.listdir()}")
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+summarizer = pipeline("summarization", model="pszemraj/led-large-book-summary")
 
 @app.route("/summarize", methods=["POST"])
 def predict():
     input_data = request.get_json().get("input_data", [])
-    summary = summarizer(input_data, do_sample=False)
+    skizo_meter = request.get_json().get("scizo_meter")
+    summary = summarizer(input_data, do_sample=True, temperature=skizo_meter)
     return jsonify(summary)
 
 if __name__ == "__main__":
