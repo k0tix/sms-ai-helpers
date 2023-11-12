66ce085e77d06b93c7f954f77b09b9d2a35f4542
Antti Nousiainen <anttinousiai@gmail.com>
2023-11-12 03:27:23 +0200

    new featsöös

--- sms-ai-backend/src/Helper.php
+++ sms-ai-backend/src/Helper.php
@@ -7,6 +7,30 @@ class Helper
     private const allowedPhones = [
         //add numbers that can be sent to
     ];
+    public static function cleanupUrlData(string $data): string
+    {
+        $dom = new DOMDocument;
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($data);
+        libxml_use_internal_errors(false);
+
+        foreach(["style", "script", "link", "footer"] as $elem) {
+            $list = $dom->getElementsByTagName($elem);
+            while ($list->length > 0) {
+                $p = $list->item(0);
+                $p->parentNode->removeChild($p);
+            }
+        }
+        $text = "";
+        $list = $dom->getElementsByTagName("div");
+        while ($list->length > 0) {
+            $p = $list->item(0);
+            $text .= $p->textContent;
+            $p->parentNode->removeChild($p);
+        }
+        $stringbody = preg_replace("/[\n\t]/", '', $text);
+        return strip_tags($stringbody);
+    }
     public static function getUrlData(string $url): string
     {
         $ch = curl_init($url);
@@ -49,5 +73,29 @@ class Helper
             $envVarSplit = explode("=", $envVar);
             $_ENV[$envVarSplit[0]] = $envVarSplit[1];
         }
+    }
+    public static function generateLLMresult(array $data, float $modifier = 1): string
+    {
+        $pagesJson = json_encode(["input_data" => $data]);
+        $url = $_ENV["LLM_API"];
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $pagesJson);
+        
+        $resp = curl_exec($curl);
+        curl_close($curl);
+
+        return $resp;
+    }
+    public static function generateB64Pdf(string $content): string
+    {
+        return "";
+    }
+    public static function generateNewEventId(): string
+    {
+        return bin2hex(random_bytes(6));
     }
 }
