72f486c6eff309250a8b343fc29444b0e9971380
Antti Nousiainen <anttinousiai@gmail.com>
2023-11-12 05:58:10 +0200

    more fixes

--- sms-ai-backend/public/index.php
+++ sms-ai-backend/public/index.php
@@ -10,10 +10,13 @@ include_once "../src/Helper.php";
 
 Helper::loadEnv("../.env");
 $db = new DB();
-
+$publicRoutes = ["/summaryGET"];
+$function = explode("?", $_SERVER['REQUEST_URI'])[0] . $_SERVER["REQUEST_METHOD"];
 if (!isset(getallheaders()["Apikey"]) || !in_array(getallheaders()["Apikey"], $db->getApiKeys())) {
-	die("Auth error");
+	if (!in_array($function, $publicRoutes)) {
+		die("Auth error");
+	}
 }
 
 $routes = new Routes($db);
-$routes->resolve(explode("?", $_SERVER['REQUEST_URI'])[0] . $_SERVER["REQUEST_METHOD"]);
+$routes->resolve($function);
