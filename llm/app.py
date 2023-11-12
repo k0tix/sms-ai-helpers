ff2213441a3f3d8e51d100d7a151be49a01b6833
Roope Kausiala <roope.kausiala@aktia.fi>
2023-11-12 04:30:59 +0200

    add parameters, logging

--- llm/app.py
+++ llm/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, jsonify, request
 from transformers import pipeline
-import os
+from evaluate import load
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
-#print(f"DIRRRR: {os.listdir()}")
 summarizer = pipeline("summarization", model="pszemraj/led-large-book-summary")
 
 @app.route("/summarize", methods=["POST"])
 def predict():
+    logging.info("Processing summary request")
     input_data = request.get_json().get("input_data", [])
     skizo_meter = request.get_json().get("scizo_meter")
-    summary = summarizer(input_data, do_sample=True, temperature=skizo_meter)
+    max_length = request.get_json().get("max_length")
+    summary = summarizer(input_data, do_sample=True, temperature=skizo_meter, max_length=max_length)
+    logging.info("Summary ready")
     return jsonify(summary)
 
 if __name__ == "__main__":
